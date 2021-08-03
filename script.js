9a6d4f0f06ff9dd7bbb68789e37c244402945239
Daniel Ye <daniel.ye@live.com>
2021-08-03 18:36:44 -0400

    formatting

--- script.js
+++ script.js
@@ -15,14 +15,21 @@ function addBookToLibrary(b) {
 const b1 = new Book("Bible", "God", 1000, true)
 const b2 = new Book("The Hobbit", "Tolkien", 965, true)
 const b3 = new Book("Heat Shock Proteins", "Joe Rogan", 203, false)
+const b4 = new Book("Metaphysics of Pepe", "Jordan Peterson", 9030, false)
+const b5 = new Book("OK BOOMER", "TikTok", 90, true)
+
 addBookToLibrary(b1)
 addBookToLibrary(b2)
 addBookToLibrary(b3)
+addBookToLibrary(b4)
+addBookToLibrary(b5)
+
 
 function displayBooks() {
   for (let i=0; i<myLibrary.length; i++) {
     let card = document.createElement("div")
     card.classList.add('card')
+    // give DOM id that relates to card index
 
     let title = document.createElement("h2")
     title.innerText = myLibrary[i].title
@@ -33,12 +40,16 @@ function displayBooks() {
     card.append(author)
 
     let pages = document.createElement("p")
-    pages.innerText = myLibrary[i].pages
+    pages.innerText = myLibrary[i].pages + " pages"
     card.append(pages)
 
-    let read = document.createElement("p")
+    let read = document.createElement("button")
     read.innerText = myLibrary[i].read ? "Read" : "Not Read"
     card.append(read)
+
+    let remove = document.createElement("button")
+    remove.innerText = "Remove"
+    card.append(remove)
     
     cardHolder.append(card)
   }

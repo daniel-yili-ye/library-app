8c26dfba056cad262dd4e5f92ee57ec292378e3f
Daniel Ye <daniel.ye@live.com>
2021-08-05 11:57:24 -0400

    refactor again

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 let myLibrary = [];
 let cardHolder = document.querySelector("div[class='card-holder']")
-const b1 = new Book("Bible", "God", 1000, true)
-const b2 = new Book("The Hobbit", "Tolkien", 965, true)
-const b3 = new Book("Heat Shock Proteins", "Joe Rogan", 203, false)
-const b4 = new Book("Metaphysics of Pepe", "Jordan Peterson", 9030, false)
-const b5 = new Book("OK BOOMER", "TikTok", 90, true)
-addBookToLibrary(b1)
-addBookToLibrary(b2)
-addBookToLibrary(b3)
-addBookToLibrary(b4)
-addBookToLibrary(b5)
+addBookToLibrary("Bible", "God", 1000, true)
+addBookToLibrary("The Hobbit", "Tolkien", 965, true)
+addBookToLibrary("Heat Shock Proteins", "Joe Rogan", 203, false)
+addBookToLibrary("Metaphysics of Pepe", "Jordan Peterson", 9030, false)
+addBookToLibrary("OK BOOMER", "TikTok", 90, true)
 displayBooks()
 let readBtn = document.querySelectorAll("button[class='read']")
 let removeBtn = document.querySelectorAll("button[class='remove']")
+let newBook = document.querySelector("button[id='new']")
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -21,40 +17,46 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-function addBookToLibrary(b) {
+function addBookToLibrary(title, author, pages, read) {
+  const b = new Book(title, author, pages, read)
   myLibrary.push(b)
 }
 
 function displayBooks() {
   for (let i=0; i<myLibrary.length; i++) {
-    let card = document.createElement("div")
-    card.classList.add('card')
-    // give DOM id that relates to card index
-
-    let title = document.createElement("h2")
-    title.innerText = myLibrary[i].title
-    card.append(title)
-
-    let author = document.createElement("p")
-    author.innerText = myLibrary[i].author
-    card.append(author)
-
-    let pages = document.createElement("p")
-    pages.innerText = myLibrary[i].pages + " pages"
-    card.append(pages)
-
-    let read = document.createElement("button")
-    read.classList.add('read')
-    read.innerText = myLibrary[i].read ? "Read" : "Not Read"
-    card.append(read)
-
-    let remove = document.createElement("button")
-    remove.classList.add('remove')
-    remove.innerText = "Remove"
-    card.append(remove)
-    
-    cardHolder.append(card)
+    createCard(i)
   }
+}
+
+function createCard(i) {
+  let card = document.createElement("div")
+  card.classList.add('card')
+  // give DOM id that relates to card index
+  card.id = i
+
+  let title = document.createElement("h2")
+  title.innerText = myLibrary[i].title
+  card.append(title)
+
+  let author = document.createElement("p")
+  author.innerText = myLibrary[i].author
+  card.append(author)
+
+  let pages = document.createElement("p")
+  pages.innerText = myLibrary[i].pages + " pages"
+  card.append(pages)
+
+  let read = document.createElement("button")
+  read.classList.add('read')
+  read.innerText = myLibrary[i].read ? "Read" : "Not Read"
+  card.append(read)
+
+  let remove = document.createElement("button")
+  remove.classList.add('remove')
+  remove.innerText = "Remove"
+  card.append(remove)
+  
+  cardHolder.append(card)
 }
 
 // read/not read event listener
@@ -63,6 +65,8 @@ readBtn.forEach(b => b.addEventListener("click", e => {
   e.target.innerText = (e.target.innerText == "Read") ? "Not Read" : "Read"
   
   // update code to actually change the myLibrary array and have the displayBooks function run again
+  myLibrary[e.target.parentNode.id].read = (myLibrary[e.target.parentNode.id].read == true) ? false : true
+  console.log(myLibrary[e.target.parentNode.id])
 }))
 
 // remove event listener
@@ -74,3 +78,6 @@ removeBtn.forEach(b => b.addEventListener("click", e => {
 }))
 
 // popup to add another book
+newBook.addEventListener("click", e => {
+  console.log(e)
+})
